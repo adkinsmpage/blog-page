e987ddc7bb1d9f96cb0b0fe0d61503c411839761
Patryk Plaza <atrykp0@gmail.com>
2021-09-14 06:48:50 +0200

    install nodeemailer and create send email endpoint in contact.ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,5 @@
-import dbConnect from "../../lib/dbConnect";
+import nodemailer from "nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +7,25 @@ export default async function handler(
 ) {
   if (req.method !== "POST") return;
   const data = req.body;
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.email,
+      pass: process.env.password,
+    },
+    secure: true,
+  });
 
-  res.status(201).send(data);
+  const mailData = {
+    from: req.body.email,
+    to: process.env.email,
+    subject: `Message From ${req.body.name}`,
+    text: `${req.body.content} from ${req.body.email}`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) res.status(404).send({ message: err.message });
+    else res.status(201).send({ message: "message sent" });
+  });
 }
