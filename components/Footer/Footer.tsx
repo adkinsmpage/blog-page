914ad5354fc30ff63eab43faa6d588b52800b42f
Patryk Plaza <atrykp0@gmail.com>
2021-09-15 06:08:59 +0200

    fix page width

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -40,57 +40,61 @@ export default function Footer() {
 
   return (
     <div className={style.wrapper}>
-      <div className={style.menu}>
-        <ul className={style.list}>
-          <li onClick={() => router.push("/")}>Blog</li>
-          <li
-            onClick={() =>
-              router.push(`${session ? "/user/profile" : "/user/login"}`)
-            }
-          >
-            {session ? "Profile" : "Login"}
-          </li>
-          <li onClick={() => router.push("https://github.com/atrykp")}>
-            Github
-          </li>
-          {session && <li onClick={logoutHandler}>Logout</li>}
-          {session && session.user.isAdmin && (
-            <>
-              <li onClick={() => router.push("/posts/newPost")}>New Post</li>
-              <li onClick={() => router.push("/users")}>All users</li>
-            </>
-          )}
-        </ul>
-      </div>
-      <div className={style.formWrapper}>
-        <p>Write something</p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="name"
-            {...register("name", { required: true })}
-          />
-          {errors.name && (
-            <p className={style.errorMessage}>Name is required</p>
-          )}
-          <input
-            type="email"
-            placeholder="email"
-            {...register("email", {
-              required: true,
-              pattern: EMAIL_VALIDATION,
-            })}
-          />
-          {errors.email && <p className={style.errorMessage}>Invalid email</p>}
-          <textarea
-            placeholder="your message"
-            {...register("content", { required: true, minLength: 2 })}
-          />
-          {errors.content && (
-            <p className={style.errorMessage}>Required (min. length 2)</p>
-          )}
-          <button className={style.formSubmitBtn}>Send</button>
-        </form>
+      <div className={style.content}>
+        <div className={style.menu}>
+          <ul className={style.list}>
+            <li onClick={() => router.push("/")}>Blog</li>
+            <li
+              onClick={() =>
+                router.push(`${session ? "/user/profile" : "/user/login"}`)
+              }
+            >
+              {session ? "Profile" : "Login"}
+            </li>
+            <li onClick={() => router.push("https://github.com/atrykp")}>
+              Github
+            </li>
+            {session && <li onClick={logoutHandler}>Logout</li>}
+            {session && session.user.isAdmin && (
+              <>
+                <li onClick={() => router.push("/posts/newPost")}>New Post</li>
+                <li onClick={() => router.push("/users")}>All users</li>
+              </>
+            )}
+          </ul>
+        </div>
+        <div className={style.formWrapper}>
+          <p>Write something</p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <p className={style.errorMessage}>Name is required</p>
+            )}
+            <input
+              type="email"
+              placeholder="email"
+              {...register("email", {
+                required: true,
+                pattern: EMAIL_VALIDATION,
+              })}
+            />
+            {errors.email && (
+              <p className={style.errorMessage}>Invalid email</p>
+            )}
+            <textarea
+              placeholder="your message"
+              {...register("content", { required: true, minLength: 2 })}
+            />
+            {errors.content && (
+              <p className={style.errorMessage}>Required (min. length 2)</p>
+            )}
+            <button className={style.formSubmitBtn}>Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
